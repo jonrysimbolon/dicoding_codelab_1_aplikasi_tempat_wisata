ea3469d0be3567516eadf335479cfd5e64d15666
jonrysimbolon <jonrysimbolon19@gmail.com>
2023-09-15 14:57:48 +0700

    Pengembangan untuk web

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -12,6 +12,158 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > 800) {
+        return DetailWebPage(place: place);
+      } else {
+        return DetailMobilePage(place: place);
+      }
+    });
+  }
+}
+
+class DetailWebPage extends StatelessWidget {
+  final TourismPlace place;
+
+  const DetailWebPage({super.key, required this.place});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Wisata bandung',
+            style: TextStyle(
+              fontFamily: 'Staatliches',
+              fontSize: 32,
+            ),
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(place.imageAsset),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    height: 150,
+                    padding: const EdgeInsets.only(bottom: 16),
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: place.imageUrls.map((url) {
+                        return Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.network(url),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  )
+                ]),
+              ),
+              const SizedBox(width: 32),
+              Expanded(
+                  child: Card(
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Container(
+                        child: Text(
+                          place.name,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 30.0,
+                            fontFamily: 'Staatliches',
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(Icons.calendar_today),
+                              const SizedBox(
+                                width: 8.0,
+                              ),
+                              Text(
+                                place.openDays,
+                                style: informationTextStyle,
+                              )
+                            ],
+                          ),
+                          const FavoriteButton(),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Icon(Icons.access_time),
+                          const SizedBox(
+                            width: 8.0,
+                          ),
+                          Text(
+                            place.openTime,
+                            style: informationTextStyle,
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 8.0,
+                      ),
+                      Row(
+                        children: [
+                          const Icon(Icons.monetization_on),
+                          const SizedBox(
+                            width: 8.0,
+                          ),
+                          Text(
+                            place.ticketPrice,
+                            style: informationTextStyle,
+                          )
+                        ],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: Text(
+                          place.description,
+                          textAlign: TextAlign.justify,
+                          style: const TextStyle(
+                              fontSize: 16.0, fontFamily: 'Oxygen'),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class DetailMobilePage extends StatelessWidget {
+  final TourismPlace place;
+
+  const DetailMobilePage({super.key, required this.place});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
       child: SingleChildScrollView(
